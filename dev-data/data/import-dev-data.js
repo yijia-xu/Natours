2f04c59986ce9687630552b353af412ebc90584f
Yijia <yijiaxu.yjx@gmail.com>
2025-06-07 15:38:21 +0800

    Enable importing/deleting data from cmd line

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const e = require('express');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {autoIndex: true})
+    .then(() => { console.log('DB connection successful!')});
+
+// Read JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// Import data into DB
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded');
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Delete all data from DB
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany(); //delete all docs
+        console.log('Data successfully deleted');
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,26 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true
     },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty level']
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     rating: {
         type: Number,
         default: 4.5,
@@ -15,7 +35,28 @@ const tourSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    startDates: [Date]
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
