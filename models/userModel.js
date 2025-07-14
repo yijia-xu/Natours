a27c51abd483fd185d6ac7c20665476cbb998dff
Yijia <yijiaxu.yjx@gmail.com>
2025-07-14 12:52:24 -0400

    Implement authentication: user signup and login

---
+++ models/userModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email address'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validator.isEmail, 'Please provide a valid email address']
+    },
+    photo: {
+        type: String,
+        default: 'default.jpg'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        select: false, // do not return password in queries
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function(el) {
+                return el === this.password; 
+            },
+            message: "Passwords do not match!"
+        }
+    }
+
+});
+
+userSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified
+    if (!this.isModified('password'))
+        return next();
+    this.password = await bcrypt.hashSync(this.password, 12);
+    this.passwordConfirm = undefined; // delete passwordConfirm field
+
+    next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
