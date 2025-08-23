58b611313534b736c240de5b71ad25881bba6be0
Yijia <yijiaxu.yjx@gmail.com>
2025-08-23 16:34:09 -0400

    Enhanced review model: add average rating calculation

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -31,12 +32,46 @@ const reviewSchema = new mongoose.Schema({
     }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
     this.populate({
         path: 'user',
         select: 'name photo'
     });
     next();
+});
+
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    console.log(stats);
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats.length > 0 ? stats[0].nRating : 0,
+        ratingsAverage: stats.length > 0 ? stats[0].avgRating : 4.5
+    }); 
+}
+reviewSchema.post('save', function () {
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.post(/^findOneAnd/, function (doc, next) {
+  console.log(`Updated doc :  ${doc}`);
+  doc.constructor.calcAverageRatings(doc.tour);
+  next();
 });
 
 const Review = mongoose.model('Review', reviewSchema);
