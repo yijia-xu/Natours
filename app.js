c96439d2a55e0e964a8eacb832f6c5857c566515
Yijia <yijiaxu.yjx@gmail.com>
2025-08-24 08:15:18 -0400

    Isuues: Error Route.get() requires a callback function but got a [object Undefined]

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,8 +12,14 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet()); // set security HTTP headers
 
@@ -47,8 +54,6 @@ app.use(hpp({
 }));
 
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     console.log(req.headers);
@@ -58,6 +63,7 @@ app.use((req, res, next) => {
 
 
 // Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
