ad0fdf441005fc1aaff7d9f340045b59ec041cb8
Yijia <yijiaxu.yjx@gmail.com>
2025-08-25 11:17:33 -0400

    feat: restructure website layout and update tour page
    - Removed the old tour.html file and integrated its content into the Pug templates.
    - Updated the footer section to include contact information and social media links.
    - Redesigned the header to use Bootstrap for better responsiveness.
    - Enhanced the overview page to display tours in a card format with additional details.
    - Created a new home page with a slider section and a product section showcasing tours.
    - Updated the login page structure for consistency with the new design.
    - Added responsive styles and improved overall layout for better user experience.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +38,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // data sanitization
 app.use(mongoSanitize());
@@ -56,7 +58,7 @@ app.use(hpp({
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    console.log(req.headers);
+    //console.log(req.cookies);
     next();
 });
 
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,19 +4,16 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1 Get tour data from collection
   const tours = await Tour.find();
-
-  // 2 Build template
-  // 3 Render that template using tour data from 1)
-  res.status(200).render('overview', {
-    title: 'All Tours',
+  res.status(200).render('home', {
+    title: 'Natours Home',
     tours
   });
 });
 
+
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1 Get the data, for the requested tour (including reviews and guides)
+  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
